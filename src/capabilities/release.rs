ec56a70078c075efa21096e731de3249c455f754
slievr <pacpellet@gmail.com>
2024-05-23 01:06:50 +0100

    Making things a little nicer and using regex rather than "." splitting to parse the semver.

--- src/capabilities/release.rs
+++ src/capabilities/release.rs
@@ -1,17 +1,73 @@
+use std::fmt;
+
 use crate::file;
 use colored::{ColoredString, Colorize};
 use inquire::{InquireError, Select};
+use regex::Regex;
+
+#[derive(Default)]
+struct SemanticVersion {
+    prefix: String,
+    major: u32,
+    minor: u32,
+    patch: u32,
+    suffix: String,
+}
+
+impl SemanticVersion {
+    fn bump_major(&self) -> SemanticVersion {
+        SemanticVersion {
+            prefix: self.prefix.clone(),
+            major: self.major + 1,
+            minor: self.minor,
+            patch: self.patch,
+            suffix: self.suffix.clone(),
+        }
+    }
+
+    fn bump_minor(&self) -> SemanticVersion {
+        SemanticVersion {
+            prefix: self.prefix.clone(),
+            major: self.major,
+            minor: self.minor + 1,
+            patch: self.patch,
+            suffix: self.suffix.clone(),
+        }
+    }
+
+    fn bump_patch(&self) -> SemanticVersion {
+        SemanticVersion {
+            prefix: self.prefix.clone(),
+            major: self.major,
+            minor: self.minor,
+            patch: self.patch + 1,
+            suffix: self.suffix.clone(),
+        }
+    }
+}
+
+impl fmt::Display for SemanticVersion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{0}{1}.{2}.{3}{4}",
+            self.prefix, self.major, self.minor, self.patch, self.suffix
+        )
+    }
+}
 
 pub fn bump() {
     let version = file::write::get_version().unwrap();
     let _files = file::search::find_local_files();
 
+    let sem_version: SemanticVersion = parse_semantic_version(&version).unwrap();
+
     let options: Vec<ColoredString> = vec![
-        bump_major(&version).green(),
-        bump_minor(&version).blue(),
-        bump_patch(&version).red(),
+        sem_version.bump_major().to_string().green(),
+        sem_version.bump_minor().to_string().blue(),
+        sem_version.bump_patch().to_string().red(),
     ];
-    let msg = format!("Current version is {version}");
+    let msg = format!("Current version is {sem_version}");
     let ans: Result<ColoredString, InquireError> = Select::new(&msg, options).prompt();
 
     match ans {
@@ -23,29 +79,21 @@ pub fn bump() {
     }
 }
 
-fn bump_major(version: &str) -> String {
-    get_bump_str(version, 0)
-}
+fn parse_semantic_version(version_str: &str) -> Option<SemanticVersion> {
+    let re = Regex::new(
+        r"^(?P<prefix>[a-zA-Z-]*)v?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?P<suffix>[-+].*)?$",
+    )
+    .unwrap();
 
-fn bump_minor(version: &str) -> String {
-    get_bump_str(version, 1)
-}
-
-fn bump_patch(version: &str) -> String {
-    get_bump_str(version, 2)
-}
-
-fn get_bump_str(version: &str, bump_type: usize) -> String {
-    let mut version_parts = version
-        .split('.')
-        .map(|part| part.to_string())
-        .collect::<Vec<String>>();
-    let major = version_parts[bump_type].parse::<u32>().unwrap() + 1;
-    version_parts[bump_type] = major.to_string();
-
-    return version_parts
-        .iter()
-        .map(|part| part.to_string())
-        .collect::<Vec<String>>()
-        .join(".");
+    re.captures(version_str).map(|caps| SemanticVersion {
+        prefix: caps
+            .name("prefix")
+            .map_or_else(String::new, |m| m.as_str().to_string()),
+        major: caps["major"].parse().unwrap(),
+        minor: caps["minor"].parse().unwrap(),
+        patch: caps["patch"].parse().unwrap(),
+        suffix: caps
+            .name("suffix")
+            .map_or_else(String::new, |m| m.as_str().to_string()),
+    })
 }
